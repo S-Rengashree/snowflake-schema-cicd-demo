404a514f06562997f3b4d36c89ca1b4a1c013765
Rengashree Soundararajan <rengashrees@gmail.com>
2026-01-30 15:43:18 +0530

    adding table to the db

--- tables/demo_table.sql
+++ tables/demo_table.sql
@@ -1,6 +1,5 @@
 CREATE OR REPLACE TABLE demo_table (
     id          NUMBER(10,0),
     name        VARCHAR(50),
-    created_at  TIMESTAMP_NTZ,
-    status varchar(50)
+    created_at  TIMESTAMP_NTZ
 );
