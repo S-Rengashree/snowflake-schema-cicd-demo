5d7b41604a2e4534577fc30c02f33ba9d0f1aacd
Rengashree Soundararajan <rengashrees@gmail.com>
2026-01-30 15:10:43 +0530

    adding status column

--- tables/demo_table.sql
+++ tables/demo_table.sql
@@ -1,5 +1,6 @@
 CREATE OR REPLACE TABLE demo_table (
     id          NUMBER(10,0),
     name        VARCHAR(50),
-    created_at  TIMESTAMP_NTZ
+    created_at  TIMESTAMP_NTZ,
+    status varchar(50)
 );
